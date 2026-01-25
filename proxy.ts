97295ee50d97ae5b23ad0abe9767b380cd6cb8e9
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2026-01-25 09:02:58 +0530

    MULTI TENANT SETUP

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,7 @@ export const config = {
   matcher: ["/((?!_next|favicon.ico).*)"],
 };
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const host = req.headers.get("host") ?? "";
   const subdomain = host.split(".")[0];
 
