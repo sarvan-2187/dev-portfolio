5f1112bffd34ab7fe4cec7370a1b7427f691af5f
NAGARAMPALLI SARVAN KUMAR <sarvankumarnagarampalli478@gmail.com>
2026-02-11 15:21:07 +0000

    Added View More on Github Button

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import ProjectsHeaderBar from '@/components/ProjectHeaderbar'
+import { Button } from '@/components/ui/button';
 import ProjectsSection from '@/sections/projects'
 import { motion } from 'framer-motion'
+import { Github } from 'lucide-react';
 import React from 'react'
 
 const ProjectsPage = () => {
@@ -25,6 +27,9 @@ const ProjectsPage = () => {
       className="px-4 lg:px-58" suppressHydrationWarning={true}>
       <ProjectsHeaderBar />
       <ProjectsSection />
+      <div className='my-8'>
+        <Button variant={"outline"} asChild><a href='https://github.com/sarvan-2187' target='_blank'><Github size={5}/>View More on Github</a></Button>
+      </div>
     </motion.div>
   )
 }
