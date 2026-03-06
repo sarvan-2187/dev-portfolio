34731e493972154a75b49448759a6f58c8b967f8
NAGARAMPALLI SARVAN KUMAR <sarvankumarnagarampalli478@gmail.com>
2026-03-06 17:52:04 +0530

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,40 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const config = {
-  matcher: "/:path*",
-};
+// export const config = {
+//   matcher: "/:path*",
+// };
 
 export function proxy(req: NextRequest) {
-  const host = req.headers.get("host") || "";
-  const url = req.nextUrl.clone();
+  // const host = req.headers.get("host") || "";
+  // const url = req.nextUrl.clone();
 
-  const domain = "sarvankumar.in";
+  // const domain = "sarvankumar.in";
 
-  let subdomain = "";
+  // let subdomain = "";
 
-  if (host.endsWith(domain)) {
-    subdomain = host.replace(`.${domain}`, "").replace(domain, "");
-  }
+  // if (host.endsWith(domain)) {
+  //   subdomain = host.replace(`.${domain}`, "").replace(domain, "");
+  // }
 
-  // main domain
-  if (!subdomain || subdomain === "www") {
-    return NextResponse.next();
-  }
+  // // main domain
+  // if (!subdomain || subdomain === "www") {
+  //   return NextResponse.next();
+  // }
 
-  const routes: Record<string, string> = {
-    projects: "/projects",
-    contact: "/contact",
-    certifications: "/certifications",
-    roadmap: "/roadmap",
-  };
+  // const routes: Record<string, string> = {
+  //   projects: "/projects",
+  //   contact: "/contact",
+  //   certifications: "/certifications",
+  //   roadmap: "/roadmap",
+  // };
 
-  const target = routes[subdomain];
+  // const target = routes[subdomain];
 
-  if (!target) {
-    url.pathname = "/404";
-    return NextResponse.rewrite(url);
-  }
+  // if (!target) {
+  //   url.pathname = "/404";
+  //   return NextResponse.rewrite(url);
+  // }
 
-  url.pathname = target;
-  return NextResponse.rewrite(url);
+  // url.pathname = target;
+  // return NextResponse.rewrite(url);
 }
